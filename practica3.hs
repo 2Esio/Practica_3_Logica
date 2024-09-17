70264d3a1749812961eb811d534542228947862c
2Esio <marco@localhost.localdomain>
2024-09-17 15:01:33 -0600

    Se agrega función clausulas | 2Esio | 17/09/2024 15:01

--- practica3.hs
+++ practica3.hs
@@ -1,3 +1,7 @@
+-- Módulos --
+module Sat where
+import Data.List (intersect, union, sort, nub)
+
 -- Pre - código --
 
 data Prop = Var String | Cons Bool | Not Prop
@@ -20,11 +24,11 @@ instance Show Prop where
 
 -- E1.1 Implementar la funcion fnn que convierte una fórmula proproposicional en su forma normal negativa.
 
-fnn :: Prop -> Prop
+--fnn :: Prop -> Prop
 
 -- E1.2 Impementar la función fnc, que convierte una fórmula proposicional en su forma normal conjuntiva. Se recomienda usar la función fnn.
 
-fnc :: Prop -> Prop
+--fnc :: Prop -> Prop
 
 -- E2.1 Crear un sinónimo Literal, que será igual a Prop por simplicidad, aunque solo deberían ser variables o negaciones de variables. 
 
@@ -37,15 +41,22 @@ type Clausula = [Literal]
 -- E2.3 Definir la función clausulas que dada una fórmula en FNC, devuelve una lista con cláusulas que la forman.
 
 clausulas :: Prop -> [Clausula]
+clausulas (And p q) = union (clausulas p) (clausulas q)
+clausulas clause = case clause of
+  clause -> [lit clause]
+  where
+    lit :: Prop -> Clausula
+    lit (Or p q) = union (lit p) (lit q)
+    lit p = [p]
 
 -- E2.4 Definir la función resolución que dadas dos cláusulas, devuelve el resolvente obtenido después de aplicar la regla de resolución binaria. Se puede asumir que se puede obtener un resolvente a partir de los argumentos.
 
-resolucion :: Clausula -> Clausula -> Clausula
+--resolucion :: Clausula -> Clausula -> Clausula 
 
 -- E3.1 Definir la función hayResolvente, que determina si es posible obtener un resolvente a partir de dos cláusulas.
 
-hayResolvente :: Clausula -> Clausula -> Bool
+--hayResolvente :: Clausula -> Clausula -> Bool
 
 -- E3.2 Definir la función saturacion, que dada una fórmula proposicional, determina si esta es satisfacible o no usando el algoritmo de saturación.
 
-saturacion :: Prop -> Bool
+--saturacion :: Prop -> Bool
